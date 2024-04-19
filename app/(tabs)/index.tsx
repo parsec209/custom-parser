5814a70c7f7451f1ce22f715d822eca0ac789bbf
parsec209 <28679581+parsec209@users.noreply.github.com>
2024-04-19 00:44:07 +0000

    fdd

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,58 +1,16 @@
-import { useContext } from "react";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Divider, Text, Button } from "react-native-paper";
 import { Link } from "expo-router";
 
-import { SelectedImagesContext } from "../../contexts/selectedImagesContext";
-import ImageSelectorIconsContainer from "../../components/ImageSelectorIconsContainer";
+import ImageSelectionsContainer from "../../components/ImageSelectionsContainer";
 
 export default function ScannerPage() {
-  const [selectedImages, setSelectedImages] = useContext(SelectedImagesContext); // as GamesContextType (example), type is defined in context file;
-
   return (
     <View style={styles.container}>
-      <Text variant="bodyMedium">Scan up to two images.</Text>
-      <Text variant="bodyMedium">Use camera or photo library.</Text>
+      <Text variant="bodyMedium">Selected up to two images per scan.</Text>
       <Divider bold style={styles.divider} />
 
-      <View style={styles.imagesContainer}>
-        <View>
-          <View style={styles.imageContainer}>
-            {selectedImages[0] ? (
-              <Image source={{ uri: selectedImages[0] }} style={styles.image} />
-            ) : (
-              <ImageSelectorIconsContainer selectedImagesIndex={0} />
-            )}
-          </View>
-          {
-            <Button
-              mode="text"
-              style={selectedImages[0] ? { opacity: 1 } : { opacity: 0 }}
-              onPress={() => setSelectedImage1(null)}
-              disabled={!selectedImages[0]}
-            >
-              Reset
-            </Button>
-          }
-        </View>
-        <View>
-          <View style={styles.imageContainer}>
-            {selectedImages[1] ? (
-              <Image source={{ uri: selectedImages[1] }} style={styles.image} />
-            ) : (
-              <ImageSelectorIconsContainer selectedImagesIndex={1} />
-            )}
-          </View>
-          <Button
-            mode="text"
-            style={selectedImages[1] ? { opacity: 1 } : { opacity: 0 }}
-            onPress={() => setSelectedImage2(null)}
-            disabled={!selectedImages[1]}
-          >
-            Reset
-          </Button>
-        </View>
-      </View>
+      <ImageSelectionsContainer />
 
       <Button
         mode="contained"
@@ -76,13 +34,5 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
-  },
-  imagesContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-
-  scanButton: {
-    marginVertical: 30,
-  },
+  }
 });
---
+++ components/ImageSelectionsContainer.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, View } from "react-native";
+
+import ImageSelectionContainer from "./ImageSelectionContainer";
+
+export default function ImageSelectionsContainer() {
+  return (
+    <View style={styles.container}>
+      <ImageSelectionContainer selectedImagesIndex={0} />
+      <ImageSelectionContainer selectedImagesIndex={1} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
